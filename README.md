2a6e6a5215db98b47b7b3b7b92fac1fb48af5ab9
Tomohiro Ueno <b6800668@planet.kanazawa-it.ac.jp>
2019-08-05 21:29:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# PythonでAIトレードを学ぶ　(ZOOM) 資料
+PythonでトレードをするAIを作ります。この会では、純粋に金融工学などで使われているアルゴリズムについて知見を深めることを目的にしています。
+
+# License
+MIT
+
+# 注意
+ソースコードや資料は、引用している部分に関しては再配布はお控えいただきたいですが、主催者(tomoueno,KatsuyukiSaegusa)が作成した資料やコードについてはMITライセンスです。煮るなり焼くなり好きに使ってください。また、再配布をせずに利用する個人利用などの目的については、自由に改変して良いです。再配布可能なコードについては、冒頭に「再配布可能」と書くようにしますので、しばしお待ちを。
+
+## Install
+Gitを使ったクローン方法
+```
+git clone https://github.com/KanazawaAIMeetup/TradePython/
+```
+Python3.5.6を使用して、ライブラリをインストールする。
+```
+pip install -r requirements.txt
+```
+
+（はじめてPythonをさわる方）
+AnacondaのPythonをインストール。3系の最新版をインストール。その後、Anaconda上での仮想環境を作成し、その環境にライブラリをインストールする。なお、仮想環境とは言うもののVM(Virtual Machine)とは全く関係ない。Anacondaは基本的に```pip install```と```conda install```を同じ環境内で併用してはいけないのが基本。ただし、matplotlibやopencvなど画面で表示する必要のあるライブラリは私の場合condaでインストールする場合が多い。(MacやWindowsの場合はpipでインストールしてもうまく動かない場合がある。)
+
+全て「y」や「yes」を選択。
+```
+conda create -n CriptoTrade python=3.5.6
+conda activate CriptoTrade
+(以下同様)
+pip install -r requirements_gpu.txt
+```
+
+## ディレクトリの説明
+
+
+```
+├── DATA
+│   ├── USDT_BTC_0.csv
+│   ├── USDT_BTC_7-25.csv
+│   ├── USDT_ETH_0.csv
+│   ├── USDT_ETH_7-25.csv
+│   ├── USDT_LTC_0.csv
+│   ├── USDT_LTC_7-25.csv
+│   ├── USDT_XMR_0.csv
+│   └── USDT_XMR_7-25.csv
+├── GetHistoricalData
+│   └── ResearchGetHistoryData.py
+├── LICENSE
+├── README.md
+├── requirements_gpu.txt
+├── TrainExample
+│   └── ChainerRLExample
+│       ├── ddqn-multiple-input
+│       │   ├── lstm.py
+│       │   ├── main_lstm.py
+│       │   ├── main.py
+│       │   ├── main_TEST_zero_fee_2000inputlen.py
+│       │   ├── main_zero_fee_2000inputlen.py
+│       │   └── README.md
+│       ├── __init__.py
+│       ├── __pycache__
+│       │   └── trade_class.cpython-35.pyc
+│       └── trade_class.py
+└── Trash
+    └── requirements_cpu_old.txt
+```
+- DATA/ 過去2年分程度の5分刻みの仮想通貨(BitCoin,Ethreum,LiteCoin,Monero)の価格データがあります。
+- GetHistoricalData/ ヒストリカルデータをpoloniexから取得するためのスクリプトがあります。
+- TrainExample/ tomouenoが学習に使用したサンプルのソースコードがあります。
+- TrainExample/trade_class.py 簡易的な取引のエミュレータがあります。手数料を加味して、単純に資産の増減を表示する程度ですが、αやβなどの指数を出す機能も今後は実装したいです。
+## 引用文献・参考文献
+- 「Poloniex - Crypto Asset Exchange」(https://poloniex.com/)
+- 「ChainerRL Visualizer」(https://github.com/chainer/chainerrl-visualizer)
+
+
---
+++ TrainExample/ChainerRLExample/trade_class.py
@@ -0,0 +1,202 @@
+#coding: utf-8
+'''
+default:2239.65016075
+after:2436.87876149
+##0.4 40%
+635.385700015
+711.099316173
+'''
+
+import numpy as np
+import poloniex
+import datetime
+import time
+import json
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class TradeClass(object):
+
+
+    def __init__(self):
+        self.trade_history = []
+        self.price_history = []
+        self.transaction_fee = 0.0015
+        self.buy_sell_fee = 0.0015
+
+    def read_cripto_watch_json(self):
+        f = open('../DATA/Min-2017-6-1.json', 'r')
+        jsonData = json.load(f)
+        return jsonData["60"]
+
+    def ReadPoloniexCSV(self):
+        import csv
+        history_data=[]
+        with open('../../../DATA/USDT_BTC_0.csv', 'r') as f:
+            reader=csv.reader(f,delimiter=',')
+            next(reader)
+            for row in reader:
+                history_data.append(float(row[1]))
+                #print(float(row[1]))
+            return history_data
+    def ReadBitflyerJson(self):
+        import csv
+        history_data=[]
+        import csv
+        with open(os.environ['HOME']+'/bitcoin/bitflyerJPY_convert.csv', 'r') as f:
+            reader=csv.reader(f,delimiter=',')
+            next(reader)  # ヘッダーを読み飛ばしたい時
+            for row in reader:
+                history_data.append(float(row[1]))
+            return history_data
+
+    def GetDataPoloniex(self):
+        polo = poloniex.Poloniex()
+        polo.timeout = 10
+        chartUSDT_BTC = polo.returnChartData('USDT_ETH', period=300, start=time.time() - 1440*60 * 500, end=time.time())#1440(min)*60(sec)=DAY
+        tmpDate = [chartUSDT_BTC[i]['date'] for i in range(len(chartUSDT_BTC))]
+        date = [datetime.datetime.fromtimestamp(tmpDate[i]) for i in range(len(tmpDate))]
+        data = [float(chartUSDT_BTC[i]['open']) for i in range(len(chartUSDT_BTC))]
+        return date ,data
+
+    def PercentageLabel(self,Xtrain,yTrain):
+        X=[]
+        Y=[]
+        for i in range(0,len(yTrain)):
+            original=Xtrain[i][-1]
+            X.append([float(val/original) for val in Xtrain[i]])
+            Y.append(float(float(yTrain[i]/Xtrain[i][-1])-1)*100*100)#%*100
+        return X,Y
+
+    def TestPercentageLabel(self,Xtrain):
+        X=[]
+        for i in range(0,len(Xtrain)):
+            original = Xtrain[-1]
+            X.append([float(val/original) for val in Xtrain])
+        return X
+
+    def buy_simple(self,money, ethereum, total_money, current_price):
+        first_money, first_ethereum, first_total_money = money, ethereum, total_money
+        spend = money * 0.1#資産全体の１割を
+        money -= spend * (1+self.buy_sell_fee)
+        if money <= 0.0:
+            return first_money,first_ethereum,first_total_money
+
+        ethereum += float(spend / current_price)
+        total_money = money + ethereum * current_price
+
+        return money, ethereum, total_money
+
+    def sell_simple(self,money, ethereum, total_money, current_price):
+            first_money, first_ethereum, first_total_money = money, ethereum, total_money
+            spend = ethereum * 0.1
+            ethereum -= spend * (1+self.buy_sell_fee)
+            if ethereum <= 0.0:
+                return first_money,first_ethereum,first_total_money
+
+            money += float(spend * current_price)
+            total_money = money + float(ethereum * current_price)
+
+            return money, ethereum, total_money
+    def pass_simple(self,money,ethereum,total_money,current_price):
+        total_money = money + float(ethereum * current_price)
+        return money,ethereum,total_money
+
+    
+    def buy(self,pred,money, ethereum, total_money, current_price):
+        first_money,first_ethereum,first_total_money = money,ethereum,total_money
+        if abs(pred) < 0.0:
+            return first_money, first_ethereum, first_total_money
+        spend = abs(money * 0.05)
+        money -= spend * 1.0000#1.0015
+        if money < 0:#もしも資産がマイナスになるようだったら、取引しない。
+            return first_money,first_ethereum,first_total_money
+        ethereum += float(spend / current_price)
+        total_money = money + ethereum * current_price
+
+        return money, ethereum, total_money
+
+
+    def SellAndCalcAmoutUsingPrediction(self,pred,money, ethereum, total_money, current_price):
+        first_money, first_ethereum, first_total_money = money, ethereum, total_money
+        spend = ethereum * 0.5 * (abs(pred)*0.1)
+        ethereum -= spend * (1.0+self.transaction_fee)#取引手数料も含めていくら分購入したかを計算。
+        if ethereum < 0.0 or abs(pred) < 0.5:##資産がマイナスになる or 予測に自信がない場合
+            #何もしない
+            return first_money,first_ethereum,first_total_money
+
+        money += float(spend * current_price)#仮想通貨を売却した分、法定通貨が増える。
+        total_money = money + float(ethereum * current_price)#仮想通貨と法定通貨両方を合計した資産を計算
+
+        return money, ethereum, total_money
+    
+    def BuyAndCalcAmoutUsingPrediction(self,pred,money, ethereum, total_money, current_price):
+        first_money, first_ethereum, first_total_money = money, ethereum, total_money
+        spend = money * (abs(pred)*0.05)#資産全体の何割を一回の取引に使うか abs(pred)にすると便利
+        money -= spend * (1.0+self.transaction_fee)#取引手数料も含めていくら分購入したかを計算。
+        if money < 0.0 or abs(pred) < 0.5:#資産がマイナスになる or 予測に自信がない場合
+            #何もしない
+            return first_money,first_ethereum,first_total_money
+
+        ethereum += float(spend / current_price)#法定通貨を消費した分、仮想通貨が増える。
+        total_money = money + ethereum * current_price#仮想通貨と法定通貨両方を合計した資産を計算
+
+        return money, ethereum, total_money
+
+
+    # 配列の長さバグかも
+    #0.0001だけだと＋30
+    #0.001*predで+200ドル
+    def simulate_trade(self,price, X_test, model):
+        money = 300
+        ethereum = 0.01
+        total_money = money + np.float64(price[0] * ethereum)
+        first_total_money = total_money
+
+        for i in range(0, len(price)):
+            print(i)
+            current_price = price[i]
+            prediction = model.predict(X_test[i])
+            pred = prediction[0]
+            if pred > 0:
+                print("buy")
+                money, ethereum, total_money = self.buy_simple(pred,money, ethereum, total_money, current_price)
+                print("money"+str(money))
+            elif pred <= 0:
+                print("sell")
+                money, ethereum, total_money = self.sell_simple(pred,money, ethereum, total_money, current_price)
+                print("money"+str(money))
+        print("FIRST"+str(first_total_money))
+        print("FINAL" + str(total_money))
+        return total_money
+
+    def update_trading_view(self, current_price, action):
+        self.price_history.append(current_price)
+        self.trade_history.append(action)
+
+    def reset_trading_view(self):
+        self.price_history=[]
+        self.trade_history=[]
+
+    def draw_trading_view(self):
+        data, date = np.array(self.price_history), np.array([idx for idx in range(0, len(self.price_history))])
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(date, data)#,marker='o'
+        ax.plot()
+
+        for num in range(0,len(self.price_history)):
+            if self.trade_history[num] == 0:
+                plt.scatter(date[num], data[num], marker="^", color="green")
+            elif self.trade_history[num] == 1:
+                plt.scatter(date[num],data[num], marker="v", color="red")
+
+        ax.set_title("Cripto Price")
+        ax.set_xlabel("Day")
+        ax.set_ylabel("Price[$]")
+        plt.grid(fig)
+        plt.show(fig)
+        self.price_history=[]
+        self.trade_history=[]
